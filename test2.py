a382e96172064900b78b4e8db6fe50bcab098ead
JunjieHu1 <zzuhujunjie@gmail.com>
2016-11-27 16:50:03 +0800

    add a line

---
+++ test2.py
@@ -0,0 +1,3 @@
+# _*_ coding: utf-8 _*_
+
+print('this is a test for add file to git version control')
---
+++ test3.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+
+print('this is a test for local git')
+
+print('I add a line in the file')
